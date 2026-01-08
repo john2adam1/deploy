a67498e66f291977de6c6f9858b6eb232a6415f8
Ali <johnjony267@gmail.com>
2026-01-08 11:41:03 +0500

    almost done, except tranlate

--- .next/dev/types/validator.ts
+++ .next/dev/types/validator.ts
@@ -90,20 +90,30 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
   type __Unused = __Check
 }
 
-// Validate ../../../app/test/exam/[examType]/page.tsx
+// Validate ../../../app/tickets/page.tsx
 {
-  type __IsExpected<Specific extends AppPageConfig<"/test/exam/[examType]">> = Specific
-  const handler = {} as typeof import("../../../app/test/exam/[examType]/page.js")
+  type __IsExpected<Specific extends AppPageConfig<"/tickets">> = Specific
+  const handler = {} as typeof import("../../../app/tickets/page.js")
   type __Check = __IsExpected<typeof handler>
   // @ts-ignore
   type __Unused = __Check
 }
 
-// Validate ../../../app/test/random/page.tsx
+
+
+
+
+
+
+// Validate ../../../app/layout.tsx
 {
-  type __IsExpected<Specific extends AppPageConfig<"/test/random">> = Specific
-  const handler = {} as typeof import("../../../app/test/random/page.js")
+  type __IsExpected<Specific extends LayoutConfig<"/">> = Specific
+  const handler = {} as typeof import("../../../app/layout.js")
   type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+cted<typeof handler>
   // @ts-ignore
   type __Unused = __Check
 }
